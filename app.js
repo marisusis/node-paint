70570a0597c27e9c7a58db332ad3d25529d7d13c
Maris Usis <marisusisme@gmail.com>
2016-09-16 11:54:07 -0400

    update for heroku

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
@@ -25,6 +27,6 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(9000, function() {
-  console.log('listening on *:9000');
+http.listen(port, function() {
+  console.log('listening on *:' + port);
 });
